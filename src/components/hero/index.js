ecface00c5e7ae658e10218038208e2462bed4c8
vahanmkrtumyanqless <vahan.mkrtumyan@qless.com>
2022-08-15 22:02:35 +0400

    Update index.js

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -37,109 +37,48 @@ class Hero extends Component {
 
     return (
       <section className="hero-area">
-        <div className="hero-social">
-          <ul>
-            <li>
-              <Link to="/">
-                <i className="fab fa-pinterest-p" />
-              </Link>
-            </li>
-            <li>
-              <Link to="/">
-                <i className="fab fa-facebook-f" />
-              </Link>
-            </li>
-            <li>
-              <Link to="/">
-                <i className="fab fa-instagram" />
-              </Link>
-            </li>
-            <li>
-              <Link to="/">
-                <i className="fab fa-twitter" />
-              </Link>
-            </li>
-          </ul>
-          <p>Follow Us</p>
-        </div>
-
         <div className="hero-slider">
-          <div className="hero_arrows">
+          {/* <div className="hero_arrows">
             <button className="button" onClick={this.previous}>
               <i className="fas fa-angle-left"></i>
             </button>
             <button className="button" onClick={this.next}>
               <i className="fas fa-angle-right"></i>
             </button>
+          </div> */}
+
+          {/* <Slider ref={(c) => (this.slider = c)} {...settings}> */}
+          {/* <div className="slide"> */}
+          <div
+            className="hero-slide-item"
+            style={{ backgroundImage: `url(${hero1})` }}
+          >
+            <div className="container">
+              <div className="hero-text">
+                <h2>
+                  Unique Architecture <br />
+                  Design Ideas
+                </h2>
+                <div className="hero-action">
+                  <Link to="/projects" className="cta-btn btn-fill">
+                    See Projects
+                  </Link>
+                  <Link to="/contact" className="cta-btn btn-border">
+                    Get Contact
+                  </Link>
+                </div>
+              </div>
+              <div className="video-main">
+                <div className="promo-video">
+                  <div className="waves-block">
+                    <div className="waves wave-1" />
+                    <div className="waves wave-2" />
+                    <div className="waves wave-3" />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <Slider ref={(c) => (this.slider = c)} {...settings}>
-            <div className="slide">
-              <div
-                className="hero-slide-item"
-                style={{ backgroundImage: `url(${hero1})` }}
-              >
-                <div className="container">
-                  <div className="hero-text">
-                    <h2>
-                      Unique Architecture <br />
-                      Design Ideas
-                    </h2>
-                    <div className="hero-action">
-                      <Link to="/projects" className="cta-btn btn-fill">
-                        See Projects
-                      </Link>
-                      <Link to="/contact" className="cta-btn btn-border">
-                        Get Contact
-                      </Link>
-                    </div>
-                  </div>
-                  <div className="video-main">
-                    <div className="promo-video">
-                      <div className="waves-block">
-                        <div className="waves wave-1" />
-                        <div className="waves wave-2" />
-                        <div className="waves wave-3" />
-                      </div>
-                    </div>
-                    <VideoModal videoId="BqI0Q7e4kbk" />
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="slide">
-              <div
-                className="hero-slide-item"
-                style={{ backgroundImage: `url(${hero2})` }}
-              >
-                <div className="container">
-                  <div className="hero-text">
-                    <h2>
-                      Unique Architecture <br />
-                      Design Ideas
-                    </h2>
-                    <div className="hero-action">
-                      <Link to="/projects" className="cta-btn btn-fill">
-                        See Projects
-                      </Link>
-                      <Link to="/contact" className="cta-btn btn-border">
-                        Get Contact
-                      </Link>
-                    </div>
-                  </div>
-                  <div className="video-main">
-                    <div className="promo-video">
-                      <div className="waves-block">
-                        <div className="waves wave-1" />
-                        <div className="waves wave-2" />
-                        <div className="waves wave-3" />
-                      </div>
-                    </div>
-                    <VideoModal videoId="WVPfu1yOOko" />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Slider>
         </div>
       </section>
     );
